6815cc6ed6271d925886c60716026cf82309ddad
AntonioRbm5 <antoniorbm5@gmail.com>
2023-11-10 09:10:53 +0300

    ajout: mise en place skeleton du projet

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
